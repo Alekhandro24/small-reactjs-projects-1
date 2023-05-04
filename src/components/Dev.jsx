659de4a610772c3bde72dbf5d81006b135863dbc
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-05-04 17:43:09 +0300

    Create Dev.jsx

---
+++ src/components/Dev.jsx
@@ -0,0 +1,13 @@
+// import { Component } from "react";
+
+// class Dev extends Component() {
+//   render() {
+//     return <h2>Hi, my Ukrainian family</h2>;
+//   }
+// }
+
+// export default Dev;
+
+export const Dev = () => {
+  return <h2>Hi, my Ukrainian family</h2>;
+};
