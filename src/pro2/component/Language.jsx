530a4895bd11ed9c86d416a0761b542353e0f155
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-05-04 17:42:34 +0300

    Create Language.jsx

---
+++ src/pro2/component/Language.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Language extends Component {
+  render() {
+    return (
+      <div>
+        <h1>List of Languages</h1>
+      </div>
+    );
+  }
+}
+
+export default Language;
