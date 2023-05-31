887d16537a71f6d125b0a6d50abc70ed9367cef6
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-05-31 21:59:42 +0300

    Update TodoList.jsx

--- src/pro2/component/MixTasks/TodoList/TodoList.jsx
+++ src/pro2/component/MixTasks/TodoList/TodoList.jsx
@@ -1,11 +1,29 @@
 import { Component } from "react";
 import ToDo from "./ToDo/ToDO";
 import todo from "../todo.json";
-
+import FormTodo from "../FormTodo/FormTodo";
+import { nanoid } from "nanoid";
 class ToDoList extends Component {
   state = {
     todoList: todo,
+    isDelete: false,
+    isCreate: false,
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.todoList.length > this.state.todoList.length) {
+      this.setState({ isDelete: true });
+      setTimeout(() => {
+        this.setState({ isDelete: false });
+      }, 1500);
+    }
+    if (prevState.todoList.length < this.state.todoList.length) {
+      this.setState({ isCreate: true });
+      setTimeout(() => {
+        this.setState({ isCreate: false });
+      }, 1500);
+    }
+  }
 
   handleCheckCompleted = (id) => {
     this.setState((prevState) => {
@@ -25,10 +43,32 @@ class ToDoList extends Component {
     });
   };
 
+  addToDo = (value) => {
+    this.setState((prev) => {
+      return {
+        todoList: [
+          ...this.state.todoList,
+          { id: nanoid(), title: value, completed: false },
+        ],
+      };
+    });
+  };
+
   render() {
     return (
       <>
         <h1>My To-Do list</h1>
+        {this.state.isDelete && (
+          <div className="alert alert-danger" role="alert">
+            To-do delete successfully!
+          </div>
+        )}
+        {this.state.isCreate && (
+          <div className="alert alert-success" role="alert">
+            To-do create successfully!
+          </div>
+        )}
+        <FormTodo addToDo={this.addToDo} />
         <ul className="list-group list-group-flush">
           {this.state.todoList.map((todo) => (
             <ToDo
