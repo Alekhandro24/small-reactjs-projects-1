678005e68e09bbff39083be6f0af80ed2823fa5a
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-06-01 14:55:22 +0300

    Update TodoList.jsx

--- src/pro2/component/MixTasks/TodoList/TodoList.jsx
+++ src/pro2/component/MixTasks/TodoList/TodoList.jsx
@@ -4,21 +4,123 @@ import todo from "../todo.json";
 import FormTodo from "../FormTodo/FormTodo";
 import { nanoid } from "nanoid";
 class ToDoList extends Component {
+  // state = {
+  //   todoList: todo,
+  //   isDelete: false,
+  //   isCreate: false,
+  // };
+  // componentDidMount() {
+  //   if (localStorage.getItem("todo")) {
+  //     this.setState({ todoList: JSON.parse(localStorage.getItem("todo")) });
+  //   }
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.todoList.length > this.state.todoList.length) {
+  //     localStorage.setItem("todo", JSON.stringify(this.state.todoList));
+  //     this.setState({ isDelete: true });
+
+  //     setTimeout(() => {
+  //       this.setState({ isDelete: false });
+  //     }, 1500);
+  //   }
+  //   if (prevState.todoList.length < this.state.todoList.length) {
+  //     localStorage.setItem("todo", JSON.stringify(this.state.todoList));
+  //     this.setState({ isCreate: true });
+
+  //     setTimeout(() => {
+  //       this.setState({ isCreate: false });
+  //     }, 1500);
+  //   }
+  // }
+
+  // handleCheckCompleted = (id) => {
+  //   this.setState((prevState) => ({
+  //     todoList: prevState.todoList.map((todo) =>
+  //       todo.id === id ? { ...todo, completed: !todo.completed } : todo
+  //     ),
+  //   }));
+  // };
+
+  // handleDelete = (id) => {
+  //   this.setState((prevState) => ({
+  //     todoList: prevState.todoList.filter((todo) => todo.id !== id),
+  //   }));
+  // };
+
+  // addToDo = (value) => {
+  //   this.setState((prev) => {
+  //     return {
+  //       todoList: [
+  //         ...prev.todoList,
+  //         { id: nanoid(), title: value, completed: false },
+  //       ],
+  //     };
+  //   });
+  // };
+
+  // render() {
+  //   return (
+  //     <>
+  //       <h1>My To-Do list</h1>
+  //       {this.state.isDelete && (
+  //         <div className="alert alert-danger" role="alert">
+  //           To-do delete successfully!
+  //         </div>
+  //       )}
+  //       {this.state.isCreate && (
+  //         <div className="alert alert-success" role="alert">
+  //           To-do create successfully!
+  //         </div>
+  //       )}
+  //       <FormTodo addToDo={this.addToDo} />
+  //       {this.state.todoList && (
+  //         <ul className="list-group list-group-flush">
+  //           {this.state.todoList.map((todo) => (
+  //             <ToDo
+  //               key={todo.id}
+  //               todo={todo}
+  //               handleCheckCompleted={this.handleCheckCompleted}
+  //               handleDelete={this.handleDelete}
+  //             />
+  //           ))}
+  //         </ul>
+  //       )}
+  //     </>
+  //   );
+  // }
+
   state = {
-    todoList: todo,
+    todoList: "",
     isDelete: false,
     isCreate: false,
   };
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidMount() {
+    // localStorage.setItem('todo', JSON.stringify(todo))
+    if (localStorage.getItem("todo"))
+      this.setState({
+        todoList: JSON.parse(localStorage.getItem("todo")),
+      });
+  }
+
+  componentDidUpdate(_, prevState) {
     if (prevState.todoList.length > this.state.todoList.length) {
-      this.setState({ isDelete: true });
+      localStorage.setItem("todo", JSON.stringify(this.state.todoList));
+      this.setState({
+        isDelete: true,
+        // todo: localStorage.getItem('todo'),
+      });
       setTimeout(() => {
         this.setState({ isDelete: false });
       }, 1500);
     }
     if (prevState.todoList.length < this.state.todoList.length) {
-      this.setState({ isCreate: true });
+      localStorage.setItem("todo", JSON.stringify(this.state.todoList));
+      this.setState({
+        isCreate: true,
+        // todo: localStorage.getItem('todo'),
+      });
       setTimeout(() => {
         this.setState({ isCreate: false });
       }, 1500);
@@ -26,29 +128,29 @@ class ToDoList extends Component {
   }
 
   handleCheckCompleted = (id) => {
-    this.setState((prevState) => {
-      return {
-        todoList: prevState.todoList.map((todo) =>
-          todo.id === id ? { ...todo, completed: !todo.completed } : todo
-        ),
-      };
-    });
+    this.setState((prevState) => ({
+      todoList: prevState.todoList.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      ),
+    }));
   };
 
   handleDelete = (id) => {
-    this.setState((prevState) => {
-      return {
-        todoList: prevState.todoList.filter((todo) => todo.id !== id),
-      };
-    });
+    this.setState((prev) => ({
+      todoList: prev.todoList.filter((todo) => todo.id !== id),
+    }));
   };
 
   addToDo = (value) => {
     this.setState((prev) => {
       return {
         todoList: [
-          ...this.state.todoList,
-          { id: nanoid(), title: value, completed: false },
+          ...prev.todoList,
+          {
+            id: nanoid(),
+            title: value,
+            completed: false,
+          },
         ],
       };
     });
@@ -59,26 +161,28 @@ class ToDoList extends Component {
       <>
         <h1>My To-Do list</h1>
         {this.state.isDelete && (
-          <div className="alert alert-danger" role="alert">
+          <div class="alert alert-danger" role="alert">
             To-do delete successfully!
           </div>
         )}
         {this.state.isCreate && (
-          <div className="alert alert-success" role="alert">
-            To-do create successfully!
+          <div class="alert alert-success" role="alert">
+            Create to-do successfully!
           </div>
         )}
         <FormTodo addToDo={this.addToDo} />
-        <ul className="list-group list-group-flush">
-          {this.state.todoList.map((todo) => (
-            <ToDo
-              key={todo.id}
-              todo={todo}
-              handleCheckCompleted={this.handleCheckCompleted}
-              handleDelete={this.handleDelete}
-            />
-          ))}
-        </ul>
+        {this.state.todoList && (
+          <ul className="list-group list-group-flush">
+            {this.state.todoList.map((todo) => (
+              <ToDo
+                key={todo.id}
+                todo={todo}
+                handleCheckCompleted={this.handleCheckCompleted}
+                handleDelete={this.handleDelete}
+              />
+            ))}
+          </ul>
+        )}
       </>
     );
   }
