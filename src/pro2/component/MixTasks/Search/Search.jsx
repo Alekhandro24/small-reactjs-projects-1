1ce83f75f267617a831589f95656dbd4fe68d698
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-06-01 14:55:19 +0300

    Create Search.jsx

---
+++ src/pro2/component/MixTasks/Search/Search.jsx
@@ -0,0 +1,46 @@
+import { Component } from "react";
+
+class Search extends Component {
+  state = {
+    value: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+    // const { target } = e;
+    // const { value } = target;
+    // this.setState({ value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <>
+        <form
+          action=""
+          className="d-flex mt-2"
+          role="search"
+          onSubmit={this.handleSubmit}
+        >
+          <input
+            onChange={this.handleChange}
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            value={this.state.value}
+          />
+          <button type="submit" className="btn btn-outline-success">
+            Search
+          </button>
+        </form>
+      </>
+    );
+  }
+}
+
+export default Search;
