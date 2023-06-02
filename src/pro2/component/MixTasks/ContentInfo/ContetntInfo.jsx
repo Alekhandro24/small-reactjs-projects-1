0b7e126eca3aa08a8c65c46481ce097fe3535eed
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-06-02 18:27:20 +0300

    Update ContetntInfo.jsx

--- src/pro2/component/MixTasks/ContentInfo/ContetntInfo.jsx
+++ src/pro2/component/MixTasks/ContentInfo/ContetntInfo.jsx
@@ -1,17 +1,63 @@
 import { Component } from "react";
+import { getNews } from "../../../../services/getNews";
+import ErrorCard from "../ErrorCard/ErrorCard";
+
+const STATUS = {
+  IDLE: "idle",
+  PENDING: "pending",
+  REJECTED: "rejected",
+  RESOLVED: "resolved",
+};
 
 class ContentInfo extends Component {
-  state = {};
+  state = {
+    news: null,
+    // isLoading: false,
+    error: "",
+    status: STATUS.IDLE,
+  };
 
   componentDidUpdate(prevProps, prevState) {
     console.log("this.props :", this.props);
     if (prevProps.searchText !== this.props.searchText) {
-      //fetch
+      this.setState({ status: STATUS.PENDING });
+      getNews(this.props.searchText)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.status === "ok") {
+            this.setState({ news: data.articles, status: STATUS.RESOLVED });
+          } else return Promise.reject(data.message);
+        })
+        .catch((error) => {
+          this.setState({ error, status: STATUS.REJECTED });
+        });
+      // .finally(() => {
+      //   this.setState({ isLoading: false });
+      // });
     }
   }
 
   render() {
-    return <></>;
+    const { news, error } = this.state;
+
+    if (this.state.status === STATUS.PENDING)
+      return (
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      );
+    else if (this.state.status === STATUS.RESOLVED)
+      return (
+        <ul>
+          {news &&
+            news.map((el) => {
+              return <li key={el.url}>{el.title}</li>;
+            })}
+        </ul>
+      );
+    else if (this.state.status === STATUS.REJECTED) {
+      return <ErrorCard>{error}</ErrorCard>;
+    }
   }
 }
 
