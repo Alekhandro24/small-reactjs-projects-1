cfbb472564a8e67fcd63def6029447eaa3909422
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-06-02 18:27:33 +0300

    Update getNews.js

--- src/services/getNews.js
+++ src/services/getNews.js
@@ -1,10 +1,10 @@
-const BASE_URL = "https://nwsapi.org/v2";
-const API_KEY = "";
+const BASE_URL = "https://newsapi.org/v2";
+const API_KEY = "feef20bb6ec8430ab253f1d0367f9ccf";
 
 export const getNews = (searchText) => {
-  fetch(`${BASE_URL}/everything?q=${searchText}`, {
-    hesders: {
-      "X-APY-KEY": API_KEY,
+  return fetch(`${BASE_URL}/everything?q=${searchText}`, {
+    headers: {
+      "X-Api-Key": API_KEY,
     },
   });
 };
