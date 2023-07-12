a892335f0ec42f8de8122f63be8e6ff8765b99e8
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-07-12 22:26:54 +0300

    Create Modal.jsx

---
+++ src/pract3GoIt/Modal/Modal.jsx
@@ -0,0 +1,46 @@
+import { Component } from "react";
+
+class Modal extends Component {
+  state = {};
+
+  componentDidMount() {
+    window.addEventListener("keydown", this.handlePressESC);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handlePressESC);
+  }
+
+  handlePressESC = (e) => {
+    if (e.code === "Escape") {
+      this.props.closeModal();
+    }
+  };
+
+  render() {
+    const { closeModal, children } = this.props;
+    return (
+      <div
+        className="modal fade show"
+        style={{ display: "block", backdropFilter: "blur(5px)" }}
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title"> Modal</h5>
+              <button
+                type="button"
+                className="btn-close"
+                aria-label="Close"
+                onClick={closeModal}
+              ></button>
+            </div>
+            <div className="modal-body">{children}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modal;
