d35e4cca5a0784cbf896183f19d2e7337845f4b0
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-07-12 22:26:30 +0300

    Create App.jsx

---
+++ src/pract3GoIt/App.jsx
@@ -0,0 +1,50 @@
+import { Component } from "react";
+import { nanoid } from "nanoid";
+
+import Header from "./Header/Header";
+import ToDoList from "./ToDoList/ToDoList";
+import Modal from "./Modal/Modal";
+import FormLogin from "./FormLogin/FormLogin";
+// import Counter from './Counter/Counter'
+
+class App extends Component {
+  state = {
+    isShowModal: false,
+  };
+
+  showModal = () => {
+    this.setState({ isShowModal: true });
+  };
+
+  closeModal = () => {
+    this.setState({ isShowModal: false });
+  };
+
+  createUser = (data) => {
+    const newUser = {
+      ...data,
+      id: nanoid(),
+    };
+    console.log("newUser :>> ", newUser);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <Header showModal={this.showModal} />
+        <ToDoList />
+        {this.state.isShowModal && (
+          <Modal closeModal={this.closeModal}>
+            <FormLogin
+              closeModal={this.closeModal}
+              createUser={this.createUser}
+            />
+          </Modal>
+        )}
+        {/* <Counter /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
