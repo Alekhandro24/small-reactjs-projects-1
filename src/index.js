8dc65c8432de550e3cbb5c685753bef9a0beed23
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-07-20 16:13:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 // import { BrowserRouter } from "react-router-dom";
 // import { App } from "./components/App";
 // import App from "./pro2/App";
-import App from "./pract3GoIt/App";
+// import App from "./pract3GoIt/App";
+import App from "./pro3/App";
 
 // import "./index.css";
 
@@ -17,15 +18,6 @@ const root = createRoot(rootElement);
 //     </BrowserRouter>
 //   </StrictMode>
 // );
-
-//"./pro2/App";
-
-// root.render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-
-//"./pract3GoIt/App";
 
 root.render(
   <StrictMode>
