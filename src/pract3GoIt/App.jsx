1d52c83c1bfbddb1338f05496cb8ae3368d5344f
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-07-20 16:13:36 +0300

    Update App.jsx

--- src/pract3GoIt/App.jsx
+++ src/pract3GoIt/App.jsx
@@ -1,50 +1,109 @@
-import { Component } from "react";
-import { nanoid } from "nanoid";
+import { Component, useState } from "react";
+// import { nanoid } from 'nanoid'
+import { Toaster } from "react-hot-toast";
 
 import Header from "./Header/Header";
-import ToDoList from "./ToDoList/ToDoList";
-import Modal from "./Modal/Modal";
-import FormLogin from "./FormLogin/FormLogin";
+import TestUseMemo from "./TestUseMemo/TestUseMemo";
+// import ToDoList from './ToDoList/ToDoList'
+// import Modal from './Modal/Modal'
+// import FormLogin from './FormLogin/FormLogin'
+// import Search from './Search/Search'
+// import ContentInfo from './ContentInfo/ContentInfo'
 // import Counter from './Counter/Counter'
 
-class App extends Component {
-  state = {
-    isShowModal: false,
-  };
+const App = () => {
+  const [isShowModal, setIsShowModal] = useState(false);
+  // const [searchText, setSearchText] = useState('')
 
-  showModal = () => {
-    this.setState({ isShowModal: true });
-  };
+  const showModal = () => setIsShowModal(true);
 
-  closeModal = () => {
-    this.setState({ isShowModal: false });
-  };
+  // const closeModal = () => setIsShowModal(false)
 
-  createUser = (data) => {
-    const newUser = {
-      ...data,
-      id: nanoid(),
-    };
-    console.log("newUser :>> ", newUser);
-  };
+  // function createUser(data) {
+  // 	const newUser = {
+  // 		...data,
+  // 		id: nanoid(),
+  // 	}
+  // 	console.log('newUser :>> ', newUser)
+  // }
 
-  render() {
-    return (
-      <div className="container">
-        <Header showModal={this.showModal} />
-        <ToDoList />
-        {this.state.isShowModal && (
-          <Modal closeModal={this.closeModal}>
-            <FormLogin
-              closeModal={this.closeModal}
-              createUser={this.createUser}
-            />
-          </Modal>
-        )}
-        {/* <Counter /> */}
-      </div>
-    );
-  }
-}
+  // const handleSearch = (searchText) => {
+  // 	setSearchText(searchText)
+  // }
+
+  return (
+    <div className="container">
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 1500,
+        }}
+      />
+      <Header showModal={showModal} />
+      {/* <Search handleSearch={handleSearch} />
+			<ContentInfo searchText={searchText} />
+			<ToDoList />
+			{isShowModal && (
+				<Modal closeModal={closeModal}>
+					<FormLogin
+						closeModal={closeModal}
+						createUser={createUser}
+					/>
+				</Modal>
+			)} */}
+      {/* <Counter /> */}
+      <TestUseMemo />
+    </div>
+  );
+};
 
 export default App;
+
+// class App extends Component {
+// 	state = {
+// 		isShowModal: false,
+// 		searchText: '',
+// 	}
+
+// 	showModal = () => {
+// 		this.setState({ isShowModal: true })
+// 	}
+
+// 	closeModal = () => {
+// 		this.setState({ isShowModal: false })
+// 	}
+
+// 	createUser = (data) => {
+// 		const newUser = {
+// 			...data,
+// 			id: nanoid(),
+// 		}
+// 		console.log('newUser :>> ', newUser)
+// 	}
+
+// 	handleSearch = (searchText) => {
+// 		this.setState({ searchText })
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div className='container'>
+// 				<Header showModal={this.showModal} />
+// 				<Search handleSearch={this.handleSearch} />
+// 				<ContentInfo searchText={this.state.searchText} />
+// 				{/* <ToDoList /> */}
+// 				{/* {this.state.isShowModal && (
+// 					<Modal closeModal={this.closeModal}>
+// 						<FormLogin
+// 							closeModal={this.closeModal}
+// 							createUser={this.createUser}
+// 						/>
+// 					</Modal>
+// 				)} */}
+// 				{/* <Counter /> */}
+// 			</div>
+// 		)
+// 	}
+// }
+
+// export default App
